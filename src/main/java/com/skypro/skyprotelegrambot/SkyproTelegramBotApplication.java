a604856496cf67f328e49df1b429b2a4fab672f0
Тиунов Георгий <34783tgs@mail.ru>
2023-04-12 22:31:09 +0300

    добавил стартовый проект

---
+++ src/main/java/com/skypro/skyprotelegrambot/SkyproTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.skyprotelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproTelegramBotApplication.class, args);
+    }
+
+}
