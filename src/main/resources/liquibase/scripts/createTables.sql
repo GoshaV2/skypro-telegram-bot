9a6deafc9cc53d8a5fe3be8be31a5c1b9e700396
hjf <marinaff-xiii@mail.ru>
2023-05-28 20:11:58 +0500

    Создание таблиц, с учетом изменений в entity. Добавление строк в таблицу.

--- src/main/resources/liquibase/scripts/createTables.sql
+++ src/main/resources/liquibase/scripts/createTables.sql
@@ -8,7 +8,7 @@ names varchar not null
 
 create table session(
 id bigserial not null primary key,
-selected_shelter_id bigint references shelter (id),
+selected_shelter_id bigint not null references shelter (id),
 has_waiting_contact boolean default false,
 is_report_sending boolean default false,
 is_first_request boolean default true
@@ -19,30 +19,41 @@ id bigserial not null primary key,
 chat_id bigint not null,
 names varchar not null,
 contact varchar not null,
-session_id bigint references session (id)
+session_id bigint not null references session (id)
 );
 
 create table shelter_user(
-shelter_id bigint references shelter (id),
-user_id bigint references users (id)
+shelter_id bigint not null references shelter (id),
+user_id bigint not null references users (id)
 );
 
 create table answer(
 id bigserial not null primary key,
 category varchar not null,
-command varchar not null,
 texts varchar not null,
 title varchar not null,
-shelter_id bigint references shelter (id)
+shelter_id bigint not null references shelter (id)
+);
+
+create table volunteer_contact(
+id bigserial not null primary key,
+chat_id bigserial not null,
+email varchar not null,
+full_name varchar not null,
+phone varchar not null,
+telegram_tag varchar not null,
+shelter_id bigint not null references shelter (id)
 );
 
 create table probation(
 id bigserial not null primary key,
-count_probation_days bigint not null,
+shelter_id bigint not null references shelter (id),
+user_id bigint not null references users (id),
 pet_name varchar not null,
 start_date date,
-shelter_id bigint references shelter (id),
-user_id bigint references users (id)
+probation_status varchar not null,
+volunteer_contact_id bigint not null references volunteer_contact (id),
+count_probation_days int not null
 );
 
 create table report(
@@ -50,15 +61,6 @@ id bigserial not null primary key,
 dates date,
 photo bytea,
 report varchar not null,
-shelter_id bigint references shelter (id),
-user_id bigint references users (id)
-);
-
-create table volunteer_contact(
-id bigserial not null primary key,
-email varchar not null,
-full_name varchar not null,
-phone varchar not null,
-telegram_tag varchar not null,
-shelter_id bigint references shelter (id)
+shelter_id bigint not null references shelter (id),
+user_id bigint not null references users (id)
 );
